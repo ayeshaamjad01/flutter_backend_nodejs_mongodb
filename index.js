5218ac67b0e07507ce8055b8562e7b8c375b8af4
ayeshaamjad01 <ayesha@ayesha.local>
2026-07-10 19:36:35 +0500

    setup for nodemon

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Hello, World!");
+ 
+
+// This is a simple Node.js application that prints "Hello, World!" to the console. To run this code, make sure you have Node.js installed on your machine. Save the code in a file named `index.js`, then open your terminal, navigate to the directory where the file is located, and run the command `node index.js`. You should see "Hello, World!" printed in the terminal.
+
+// to run this code everytime you save the file , I am installing nodemon globally using the command `npm install -g nodemon`. After installing nodemon, you can run the command `nodemon index.js` in your terminal. This will automatically restart the application every time you save changes to the file, allowing for a smoother development experience.
